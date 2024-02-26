66d88f0e9677342b02ff3b3107f9262a17f6c460
Abdulquadri (Ola) Olatunbosun <olaolat@hotmail.com>
2024-02-26 13:38:11 +0000

    add unit tests for NotesRepository

---
+++ test/src/features/notes/data/fake_notes.dart
@@ -0,0 +1,43 @@
+import 'package:noteworthy/features/notes/domain/note.dart';
+
+Note note1 = const Note(
+    title: "Plan for Monday",
+    color: "#ffda1",
+    group: "important",
+    uid: "user1",
+    id: "note1",
+    content: "Wash my car");
+
+Note note2 = const Note(
+    title: "Plan for Tuesday",
+    color: "#ffda1",
+    group: "important",
+    uid: "user1",
+    id: "note2",
+    content: "Clean my room");
+
+Note note3 = const Note(
+    title: "Plan for Wednesday",
+    color: "#ffda1",
+    group: "important",
+    uid: "user1",
+    id: "note3",
+    content: "Go shopping");
+
+Note note4 = const Note(
+    title: "Plan for Thursday",
+    color: "#ffda1",
+    group: "important",
+    uid: "user1",
+    id: "note4",
+    content: "Read a book");
+
+Note note5 = const Note(
+    title: "Plan for Friday",
+    color: "#ffda1",
+    group: "important",
+    uid: "user1",
+    id: "note5",
+    content: "Wash my clothes");
+
+List<Note> fakeNotes = [note1, note2, note3, note4, note5];
--- test/src/features/notes/data/notes_repository_test.dart
+++ test/src/features/notes/data/notes_repository_test.dart
@@ -1,39 +1,43 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:noteworthy/features/notes/data/notes_repository.dart';
-import 'package:noteworthy/features/notes/domain/note.dart';
-
-const notesCollection = 'notes';
+import 'fake_notes.dart';
 
 void main() {
-  Note note = const Note(
-      title: "Title1",
-      color: "blue",
-      group: "important",
-      uid: "user1",
-      id: "note1",
-      content: "Content1");
-  List<Note> notes = [note];
-  NotesRepository makeNotesRepository() {
-    final fakeFirestore = FakeFirebaseFirestore();
-    return NotesRepository(fakeFirestore);
-  }
+  FakeFirebaseFirestore fakeFirestore = FakeFirebaseFirestore();
 
-  setUp(() {});
+  setUp(() {
+    // add notes to firstore collection
+    final collection = fakeFirestore.collection('notes');
+    fakeNotes.forEach((item) async {
+      await collection.doc(item.id).set(item.toMap());
+    });
+  });
 
   group("NotesRepository", () {
-    test("fetch note from firestore", () async {
-      final notesRepository = makeNotesRepository();
-      await fakeFirestore
-          .collection(notesCollection)
-          .doc('note1')
-          .set(note.toMap());
-      expect(await notesRepository.fetchNote("note1"), note);
-    });
-    test("fetch invalid note from firestore", () async {
-      final fakeFirestore = FakeFirebaseFirestore();
+    test("fetchNote('note1') returns correct note", () async {
       final notesRepository = NotesRepository(fakeFirestore);
-      expect(await notesRepository.fetchNote("note100"), note);
+      expect(await notesRepository.fetchNote("note1"), note1);
+    });
+    test("fetchNote('note100') returns null", () async {
+      final notesRepository = NotesRepository(fakeFirestore);
+      expect(await notesRepository.fetchNote("note100"), null);
+    });
+    test("fetchUserNotes('user1') returns global list", () async {
+      final notesRepository = NotesRepository(fakeFirestore);
+      expect(await notesRepository.fetchUserNotes('user1'), fakeNotes);
+    });
+    test("watchNote('note1') returns correct item", () {
+      final notesRepository = NotesRepository(fakeFirestore);
+      expect(notesRepository.watchNote('note1'), emits(note1));
+    });
+    test("watchNote('note001') returns null", () {
+      final notesRepository = NotesRepository(fakeFirestore);
+      expect(notesRepository.watchNote('note100'), emits(null));
+    });
+    test("watchUserNotes('user1') returns global list", () {
+      final notesRepository = NotesRepository(fakeFirestore);
+      expect(notesRepository.watchUserNotes('user1'), emits(fakeNotes));
     });
   });
 }
